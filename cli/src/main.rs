5bac396bc22de513ee7b10d1c3743800d74da359
Dworv <dwarvyt@gmail.com>
2023-06-11 14:42:19 -0700

    chore: remove main test and add todo mark

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,9 +1,3 @@
-use std::io::{stdin, stdout, BufReader};
-
-use brainfuck::Brainfuck;
-use insolent::Language;
-
 fn main() {
-    let code = "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.".to_string();
-    Brainfuck::interpret(&Brainfuck, &mut BufReader::new(&mut code.as_bytes()), Box::new(&mut stdin()), Box::new(&mut stdout())).unwrap();
+    todo!()
 }
