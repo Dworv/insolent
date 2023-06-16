1c343f5a4173695bd4bc72f8da14091c14f7b4f5
Dworv <dwarvyt@gmail.com>
2023-06-16 09:00:46 -0700

    test: move all language tests to integration tests

--- brainfuck/tests/integration_test.rs
+++ brainfuck/tests/integration_test.rs
@@ -19,3 +19,60 @@ fn run_staircase() {
     let mut code = &include_bytes!("staircase.b")[..];
     Brainfuck.interpret(&mut BufReader::new(&mut code), Box::new(&mut empty()), Box::new(&mut sink())).unwrap();
 }
+
+
+#[test]
+fn all_symbols_run() {
+    let mut code = &include_bytes!("all_symbols.b")[..];
+    Brainfuck.interpret(&mut BufReader::new(&mut code), Box::new(&mut empty()), Box::new(&mut sink())).unwrap();
+}
+
+#[test]
+fn encoding_works() {
+    let mut code = &include_bytes!("encodings.b")[..];
+    let mut output = String::from("                    ");
+    Brainfuck.interpret(&mut BufReader::new(&mut code), Box::new(&mut empty()), Box::new(&mut unsafe { output.as_bytes_mut() })).unwrap();
+    assert_eq!(
+        output.trim_end().to_string(),
+        " !\"#$%&'()"
+    )
+}
+
+#[test]
+fn hello_word_runs() {
+    let mut code = &include_bytes!("hello_world.b")[..];
+    Brainfuck.interpret(&mut BufReader::new(&mut code), Box::new(&mut empty()), Box::new(&mut sink())).unwrap();
+}
+
+#[test]
+fn hello_word_runs_correctly() {
+    let mut code = &include_bytes!("hello_world.b")[..];
+    let mut output = String::from("                    ");
+    Brainfuck.interpret(&mut BufReader::new(&mut code), Box::new(&mut empty()), Box::new(&mut unsafe { output.as_bytes_mut() })).unwrap();
+    assert_eq!(
+        output.trim().to_string(),
+        "Hello World!"
+    )
+}
+
+#[test]
+fn character_comments_ignored() {
+    let mut code = &include_bytes!("commented_code.b")[..];
+    let mut output = String::from("                    ");
+    Brainfuck.interpret(&mut BufReader::new(&mut code), Box::new(&mut empty()), Box::new(&mut unsafe { output.as_bytes_mut() })).unwrap();
+    assert_eq!(
+        output.trim().to_string(),
+        "!"
+    )
+}
+
+#[test]
+fn first_loop_comments_ignored() {
+    let mut code = &include_bytes!("commented_code_2.b")[..];
+    let mut output = String::from("                    ");
+    Brainfuck.interpret(&mut BufReader::new(&mut code), Box::new(&mut empty()), Box::new(&mut unsafe { output.as_bytes_mut() })).unwrap();
+    assert_eq!(
+        output.trim().to_string(),
+        "#"
+    )
+}
