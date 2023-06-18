72a382b69ad0f810e03d2658bef96591fa545cb4
Dworv <dwarvyt@gmail.com>
2023-06-18 14:09:13 -0700

    fix: cli now errors properly

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,8 +1,12 @@
-use std::{process::exit, io::{stdin, stdout, BufReader}, path::PathBuf, fs::File};
+use std::{
+    fs::File,
+    io::{stdin, stdout, BufReader},
+    path::PathBuf
+};
 
 use brainfuck::Brainfuck;
-use clap::{Parser, Command, FromArgMatches as _, Args};
-use insolent::Language;
+use clap::{Args, Command, FromArgMatches as _, Parser};
+use insolent::{Language, LanguageErrorKind::*};
 
 /// A tool to interpret code written in esotoric programming languages
 #[derive(Parser, Debug)]
@@ -12,17 +16,13 @@ struct Cli {
     filename: PathBuf,
 
     #[arg(short, long)]
-    lang: String
+    lang: String,
 }
 
 fn main() {
-    let languages: Vec<&dyn Language> = vec![
-        &Brainfuck
-    ];
+    let languages: Vec<&dyn Language> = vec![&Brainfuck];
 
-    let names: Vec<&'static str> = vec![
-        "brainfuck"
-    ];
+    let names: Vec<&'static str> = vec!["brainfuck"];
 
     let cli = get_cli(names);
 
@@ -34,17 +34,28 @@ fn main() {
     }
 
     if let Some(lang) = lang {
-        lang.interpret(&mut BufReader::new(&mut File::open(cli.filename).unwrap()), Box::new(&mut stdin()), Box::new(&mut stdout())).unwrap();
+        let res = lang.interpret(
+            &mut BufReader::new(&mut File::open(cli.filename).unwrap()),
+            Box::new(&mut stdin()),
+            Box::new(&mut stdout()),
+        );
+        if let Err(e) = res {
+            match e.kind {
+                Syntax => {
+                    eprintln!("Syntax error: {}\nLine {}, column {}", e.message, e.line, e.column)
+                },
+                Runtime => {
+                    eprintln!("Runtime error: {}\nLine {}, column {}", e.message, e.line, e.column)
+                },
+            }
+        }
     }
 }
 
 fn get_cli(allowed_langs: Vec<&'static str>) -> Cli {
-    let cli = Cli::augment_args(Command::new("")).mut_arg("lang", |arg| arg.value_parser(allowed_langs)); 
+    let cli =
+        Cli::augment_args(Command::new("")).mut_arg("lang", |arg| arg.value_parser(allowed_langs));
     Cli::from_arg_matches(&cli.get_matches())
         .map_err(|err| err.exit())
         .unwrap()
-}
-
-fn deduce_lang(filename: &str) {
-    
 }
