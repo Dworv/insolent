1fbdf24918f4cf8d466d79863b76b57f5fc9713e
Dworv <dwarvyt@gmail.com>
2023-07-12 14:19:38 -0700

    feat: basic interpreting structure

--- fish/src/interpreter.rs
+++ fish/src/interpreter.rs
@@ -2,10 +2,57 @@ use std::io::{ErrorKind::UnexpectedEof, Read, Write, BufReader};
 
 use insolent::{LanguageError, LanguageErrorKind};
 
+use crate::grid::Grid;
+
 pub fn interpret(
     chars: &mut BufReader<&mut dyn Read>,
     input: Box<&mut dyn Read>,
     output: Box<&mut dyn Write>,
 ) -> Result<(), LanguageError> {
-    todo!()
+    let grid = Grid::new(chars);
+    let mut state = State::default();
+
+    loop {
+        match state.mode {
+            Mode::Normal => {
+                
+            },
+            Mode::Characters => todo!(),
+        }
+    }
+}
+
+#[derive(Default)]
+struct State {
+    row: usize,
+    col: usize,
+    dir: Direction,
+    mode: Mode
+}
+
+#[derive(Default)]
+enum Mode {
+    #[default]
+    Normal,
+    Characters
+}
+
+#[derive(Default)]
+enum Direction {
+    #[default]
+    Right,
+    Up,
+    Down,
+    Left,
+}
+
+impl Direction {
+    fn offset(&self) -> (isize, isize) {
+        match self {
+            Direction::Up => (-1, 0),
+            Direction::Right => (0, 1),
+            Direction::Down => (1, 0),
+            Direction::Left => (-1, 0)
+        }
+    }
 }
